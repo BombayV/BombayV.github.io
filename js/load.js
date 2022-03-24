04a22ed2488da184ccf3861607e90bfe4e0fcb7b
Bombay <OMGmsebastian@gmail.com>
2022-03-24 13:49:31 -0400

    temporal eh

--- js/load.js
+++ js/load.js
@@ -6,15 +6,16 @@ const images = doc.getElementsByClassName("images-selected");
 const blur = doc.getElementById('blur')
 
 let fullscreen = false
+let current = 0
 
-this.window.addEventListener('load', ()=> {
-    logo.style.animation = 'fadeOut 5s forwards';
-    wrapper.style.animation='fadeIn 5s forwards';
+window.addEventListener('load', ()=> {
+    logo.style.animation = 'fadeOut 2.5s forwards';
+    wrapper.style.animation='fadeIn 2.5s forwards';
     setTimeout(function() {
         logo.style.display='none',
         logo.style.animation='none',
         console.log('%c Page loaded!', 'color: #B3E820');
-    }, 5100)
+    }, 2500)
 
     for (let i=0; i < images.length; i++) {
         images[i].addEventListener('click', increaseSize, false);
@@ -23,6 +24,7 @@ this.window.addEventListener('load', ()=> {
 
 const increaseSize = function() {
     if (!fullscreen) {
+        current = this.id
         this.classList.add('fullscreen');
         this.classList.remove('images-selected');
         blur.style.display='block'
@@ -32,9 +34,20 @@ const increaseSize = function() {
         this.classList.add('images-selected');
         blur.style.display='none'
         fullscreen = false
+        current = 0
     }
 };
 
+blur.addEventListener('click', () => {
+  if (fullscreen) {
+    doc.getElementById(current).classList.remove('fullscreen');
+    doc.getElementById(current).classList.add('images-selected');
+    blur.style.display='none'
+    fullscreen = false
+  }
+})
+
 for (let i=0; i < images.length; i++) {
     images[i].addEventListener('click', increaseSize, false);
+    images[i].id = i
 }
